fa723473752e901ab13d82e74fd7243d5a4b7dad
Rushi G <rushi_g@tekditechnologies.com>
2025-02-05 14:03:35 +0530

    scp repo added in nx workspace

--- mfes/scp-teacher-repo/build/static/ssr/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages/_app.tsx -> @/components/InstallPopup\":{\"id\":\"pages/_app.tsx -> @/components/InstallPopup\",\"files\":[\"static/chunks/src_components_InstallPopup_tsx.js\"]}}"
