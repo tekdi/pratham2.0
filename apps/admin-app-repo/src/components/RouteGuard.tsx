a7aa0dabebcb68629867869c1dcac387fb12200f
ttpl-rt-217 <shreyas_s@techjoomla.com>
2025-02-28 16:50:53 +0530

    Issue task PS-4077 : roles changes in front-end

--- apps/admin-app-repo/src/components/RouteGuard.tsx
+++ apps/admin-app-repo/src/components/RouteGuard.tsx
@@ -45,7 +45,7 @@ const RouteGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const user = JSON.parse(adminInfo);
 
     const allowedPaths = ["/workspace","/course-planner", "/subjectDetails","/stateDetails" ];
-    const notAllowedPathsForCentralAdmin = ["/team-leader","/faciliator", "/learners","/centers" ];
+    const notAllowedPathsForCentralAdmin = ["/team-leader", "/faciliator", "/learners", "/centers", "/certificate-issuance", "/mentor", "/mentor-leader"  ];
     
     const isWorkspaceContent = router.pathname.startsWith("/workspace");
     const coursePlannerPaths = [
@@ -65,10 +65,31 @@ const RouteGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       "/course-hierarchy"
     ];
 
-    const youthNetAllowed = [
-      "/mentor",
-      "/mentor-leader",
-      "/support-request"
+    // const youthNetAllowed = [
+    //   "/mentor",
+    //   "/mentor-leader",
+    //   "/support-request"
+    // ];
+
+    const youthNetNotAllowed = [
+      '/centers',
+      '/programs',
+      '/team-leader',
+      '/faciliator',
+      '/learners',
+      '/certificate-issuance',
+      '/state',
+      '/district',
+      '/block',
+      '/notification-templates',
+      '/course-planner',
+      '/stateDetails',
+      '/subjectDetails',
+      '/importCsv',
+      '/resourceList',
+      '/play/content/[identifier]',
+      '/workspace',
+      '/course-hierarchy/[identifier]'
     ];
 
 
@@ -111,14 +132,14 @@ const RouteGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     
 
     if (
-      (user.role === Role.ADMIN || user.role === Role.CENTRAL_ADMIN) &&
-      user?.tenantData[0]?.tenantName === "Second Chance Program" &&
-      (router.pathname === "/mentor" || youthNetAllowed.includes(router.pathname))
+      user.role === Role.ADMIN &&
+      user?.tenantData[0]?.tenantName === "YouthNet" &&
+      youthNetNotAllowed.some(route => router.pathname.startsWith(route))
     ) {
       router.push("/unauthorized");
     }
 
-    if ((((user.role === Role.ADMIN && user?.tenantData[0]?.tenantName == "Second Chance Program") || (user.role === Role.CENTRAL_ADMIN &&  user?.tenantData[0]?.tenantName == "Second Chance Program")) && (allowedPaths.includes(router.pathname) || isWorkspaceContent || isCoursePlannerContent)) ||  (user.role === Role.ADMIN && (router.pathname === "/programs" ||router.pathname === "/notification-templates" ))) {
+    if ((((user.role === Role.ADMIN && user?.tenantData[0]?.tenantName == "Second Chance Program") || (user.role === Role.CENTRAL_ADMIN && user?.tenantData[0]?.tenantName == "Second Chance Program")) && (allowedPaths.includes(router.pathname) || isWorkspaceContent || isCoursePlannerContent)) || (user.role === Role.ADMIN && (router.pathname === "/programs" || router.pathname === "/notification-templates" || router.pathname === "/mentor" || router.pathname === "/mentor-leader" ))) {
     
       if (router.pathname !== "/login" && router.pathname !== "/logout" && router.pathname !== "/edit-password") {
 
