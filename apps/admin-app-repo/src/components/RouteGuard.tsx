5ce77fad93c38a5ef5ed4452da8fe8480f7c5a64
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-03-05 15:47:23 +0530

    Issue feat: move certificate to youthnet program

--- apps/admin-app-repo/src/components/RouteGuard.tsx
+++ apps/admin-app-repo/src/components/RouteGuard.tsx
@@ -77,7 +77,6 @@ const RouteGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       '/team-leader',
       '/faciliator',
       '/learners',
-      '/certificate-issuance',
       '/state',
       '/district',
       '/block',
@@ -139,10 +138,14 @@ const RouteGuard: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       router.push("/unauthorized");
     }
 
-    if ((((user.role === Role.ADMIN && user?.tenantData[0]?.tenantName == youthNetTenantName.SECOND_CHANCE_PROGRAM) || (user.role === Role.CENTRAL_ADMIN && user?.tenantData[0]?.tenantName == youthNetTenantName.SECOND_CHANCE_PROGRAM)) && (allowedPaths.includes(router.pathname) || isWorkspaceContent || isCoursePlannerContent)) || (user.role === Role.ADMIN && (router.pathname === "/programs" || router.pathname === "/notification-templates"))) {
-    
+    if ((((user.role === Role.ADMIN && user?.tenantData[0]?.tenantName == youthNetTenantName.SECOND_CHANCE_PROGRAM) || (user.role === Role.CENTRAL_ADMIN && user?.tenantData[0]?.tenantName == youthNetTenantName.SECOND_CHANCE_PROGRAM)) && (allowedPaths.includes(router.pathname) || isWorkspaceContent || isCoursePlannerContent)) || (user.role === Role.ADMIN && (router.pathname === "/programs"  || router.pathname === "/notification-templates"))) {
+       
       if (router.pathname !== "/login" && router.pathname !== "/logout" && router.pathname !== "/edit-password") {
 
+        router.push("/unauthorized");
+      }
+      if(user?.tenantData[0]?.tenantName == youthNetTenantName.SECOND_CHANCE_PROGRAM &&  router.pathname === "/certificate-issuance")
+      {
         router.push("/unauthorized");
       }
     }
