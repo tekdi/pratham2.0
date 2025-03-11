15970d69e49e654c916daaf9309802229e9565d6
Rajnish Dargan <rajnish.code30@gmail.com>
2025-03-11 19:15:55 +0530

    Issue #PS-4226: feat: Reverting file changes

--- apps/admin-app-repo/src/components/DynamicForm/DynamicForm.tsx
+++ apps/admin-app-repo/src/components/DynamicForm/DynamicForm.tsx
@@ -187,7 +187,6 @@ const DynamicForm = ({
         keysToRemove.forEach((key) => delete updatedData[key]);
         return updatedData;
       };
-
       let updatedFormData = removeDependentKeys(
         temp_prefilled_form,
         dependentKeys
@@ -195,7 +194,7 @@ const DynamicForm = ({
       // console.log('updatedFormData', updatedFormData);
       setFormData(updatedFormData);
 
-      // Prefill other dependent keys
+      //prefill other dependent keys
       const filterDependentKeys = (
         formData: Record<string, any>,
         keysToKeep: string[]
@@ -204,7 +203,6 @@ const DynamicForm = ({
           Object.entries(formData).filter(([key]) => keysToKeep.includes(key))
         );
       };
-
       let filteredFormData = filterDependentKeys(
         temp_prefilled_form,
         dependentKeys
@@ -212,7 +210,6 @@ const DynamicForm = ({
       console.log('filteredFormData', filteredFormData);
       const filteredFormDataKey = Object.keys(filteredFormData);
       console.log('filteredFormDataKey', filteredFormDataKey);
-
       let filterDependentApis = [];
       for (let i = 0; i < filteredFormDataKey.length; i++) {
         filterDependentApis.push({
@@ -233,6 +230,8 @@ const DynamicForm = ({
       };
 
       const fetchDependentApis = async () => {
+        // Filter only the dependent APIs based on the changed field
+        const dependentApis = workingSchema;
         try {
           console.log('dependentApis dependentApis', dependentApis);
           const apiRequests = dependentApis.map((realField) => {
@@ -246,7 +245,6 @@ const DynamicForm = ({
             const changedFieldValue = temp_prefilled_form[changedField];
 
             // Replace "**" in the payload with changedFieldValue
-
             const updatedPayload = JSON.parse(
               JSON.stringify(api.payload).replace(/\*\*/g, changedFieldValue)
             );
@@ -258,18 +256,104 @@ const DynamicForm = ({
               ...(api.method === 'POST' && { data: updatedPayload }),
             };
 
+            if (key) {
+              const changedField = key;
+
+              // console.log(`Field changed: ${changedField}, New Value: ${formData[changedField]}`);
+              // console.log('dependentSchema', dependentSchema);
+              const workingSchema1 = dependentSchema?.filter(
+                (item) => item.api && item.api.dependent === changedField
+              );
+              // console.log('workingSchema1', workingSchema1);
+              if (workingSchema1.length > 0) {
+                const changedFieldValue = temp_prefilled_form[changedField];
+
+                const getNestedValue = (obj, path) => {
+                  if (path === '') {
+                    return obj;
+                  } else {
+                    return path
+                      .split('.')
+                      .reduce((acc, key) => acc && acc[key], obj);
+                  }
+                };
+
+                const fetchDependentApis = async () => {
+                  // Filter only the dependent APIs based on the changed field
+                  const dependentApis = workingSchema1;
+                  try {
+                    const apiRequests = dependentApis.map((field) => {
+                      const { api, key } = field;
+
+                      // Replace "**" in the payload with changedFieldValue
+                      const updatedPayload = JSON.parse(
+                        JSON.stringify(api.payload).replace(
+                          /\*\*/g,
+                          changedFieldValue
+                        )
+                      );
+
+                      const config = {
+                        method: api.method,
+                        url: api.url,
+                        headers: { 'Content-Type': 'application/json' },
+                        ...(api.method === 'POST' && { data: updatedPayload }),
+                      };
+
+                      return axios(config).then((response) => ({
+                        fieldKey: field.key,
+                        data: getNestedValue(
+                          response.data,
+                          api.options.optionObj
+                        ),
+                      }));
+                    });
+
+                    const responses = await Promise.all(apiRequests);
+                    // console.log('API Responses:', responses);
+                    setFormSchema((prevSchema) => {
+                      const updatedProperties = { ...prevSchema.properties };
+                      responses.forEach(({ fieldKey, data }) => {
+                        // console.log('Data:', data);
+                        // console.log('fieldKey:', fieldKey);
+                        let label =
+                          prevSchema.properties[fieldKey].api.options.label;
+                        let value =
+                          prevSchema.properties[fieldKey].api.options.value;
+                        updatedProperties[fieldKey] = {
+                          ...updatedProperties[fieldKey],
+                          enum: data.map((item) => item?.[value].toString()),
+                          enumNames: data.map((item) =>
+                            item?.[label].toString()
+                          ),
+                        };
+                      });
+
+                      return { ...prevSchema, properties: updatedProperties };
+                    });
+                  } catch (error) {
+                    console.error('Error fetching dependent APIs:', error);
+                  }
+                };
+
+                // Call the function
+                fetchDependentApis();
+              }
+            }
+
             return axios(config).then((response) => ({
               fieldKey: key,
               data: getNestedValue(response.data, api.options.optionObj),
             }));
           });
 
-          const responses = await Promise.all(apiRequests.filter(Boolean)); // Remove null requests
+          const responses = await Promise.all(apiRequests);
           console.log('API Responses:', responses);
-
           setFormSchema((prevSchema) => {
             const updatedProperties = { ...prevSchema.properties };
             responses.forEach(({ fieldKey, data }) => {
+              console.log('Data:', data);
+              console.log('fieldKey:', fieldKey);
               let label = prevSchema.properties[fieldKey].api.options.label;
               let value = prevSchema.properties[fieldKey].api.options.value;
               updatedProperties[fieldKey] = {
@@ -286,15 +370,21 @@ const DynamicForm = ({
         }
       };
 
+      // Call the function
       fetchDependentApis();
+
+      //setFormData
+      setFormData(temp_prefilled_form);
 
       function getSkipKeys(skipHideObject, formData) {
         let skipKeys = [];
+
         Object.keys(skipHideObject).forEach((key) => {
           if (formData[key] && skipHideObject[key][formData[key]]) {
             skipKeys = skipKeys.concat(skipHideObject[key][formData[key]]);
           }
         });
+
         return skipKeys;
       }
 
@@ -303,6 +393,7 @@ const DynamicForm = ({
       let updatedUISchema = formUiSchemaOriginal;
       function hideFieldsInUISchema(uiSchema, fieldsToHide) {
         const updatedUISchema = { ...uiSchema };
+
         fieldsToHide.forEach((field) => {
           if (updatedUISchema[field]) {
             updatedUISchema[field] = {
@@ -312,15 +403,11 @@ const DynamicForm = ({
             };
           }
         });
+
         return updatedUISchema;
       }
-
       const hiddenUISchema = hideFieldsInUISchema(updatedUISchema, skipKeys);
       setFormUiSchema(hiddenUISchema);
-    } else {
-      // When dependentApis is empty, ensure formData is still set
-      console.log('No dependent APIs found, setting form data directly');
-      setFormData(temp_prefilled_form);
     }
     //Code patch: bug solved for prefilled dependent field options render
     setIsRenderCompleted(true);
@@ -414,7 +501,7 @@ const DynamicForm = ({
               }));
             });
 
-            const responses = await Promise.all(apiRequests.filter(Boolean)); // Remove null requests
+            const responses = await Promise.all(apiRequests);
             // console.log('API Responses:', responses);
             setFormSchema((prevSchema) => {
               const updatedProperties = { ...prevSchema.properties };
