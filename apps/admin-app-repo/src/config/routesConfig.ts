aa3567e095b1d623ef0c39c51165ecc6890725b1
ttpl-rt-217 <shreyas_s@techjoomla.com>
2025-03-11 16:19:00 +0530

    Issue task PS-4229 : Dynamic menu ,role , routes, changes

--- apps/admin-app-repo/src/config/routesConfig.ts
+++ apps/admin-app-repo/src/config/routesConfig.ts
@@ -10,68 +10,33 @@ export const PUBLIC_ROUTES = [
 ];
 export const ROLE_BASED_ROUTES = {
   [TenantName.SECOND_CHANCE_PROGRAM]: {
-    [Role.ADMIN]: [
-      '/center',
-      '/team-leader',
-      '/faciliator',
-      '/learners',
-      '/district',
-      '/block',
-      '/village',
-      '/support-request',
-      '/batch',
-    ],
+    [Role.ADMIN]: [],
     [Role.CENTRAL_ADMIN]: [
-      '/team-leader',
-      '/faciliator',
-      '/learners',
-      '/state',
-      '/district',
-      '/block',
-      '/village',
-      '/programs',
-      '/notification-templates',
       '/notification-templates/create',
       '/notification-templates/update/[identifier]',
-      '/support-request',
+      
     ],
     [Role.CCTA]: [
-      '/support-request',
-      '/course-planner',
+      
       '/subjectDetails',
       '/importCsv',
-      '/workspace',
+    
     ],
     [Role.SCTA]: [
-      '/support-request',
-      '/course-planner',
+      
       '/subjectDetails',
       '/importCsv ',
-      '/workspace',
     ],
   },
   [TenantName.YOUTHNET]: {
     [Role.ADMIN]: [
-      '/mentor',
-      '/mentor-leader',
-      '/certificate-issuance',
-      '/support-request',
-      '/support-request',
-      '/district',
-      '/block',
-      '/village',
     ],
     [Role.CENTRAL_ADMIN]: [
-      '/support-request',
-      '/mentor',
-      '/mentor-leader',
-      '/state',
-      '/district',
-      '/block',
-      '/village',
+      
+      
     ],
      
-    [Role.CCTA]: ['/support-request' ,  '/workspace',],
-    [Role.SCTA]: ['/support-request',  '/workspace',],
+    [Role.CCTA]: [ ],
+    [Role.SCTA]: [ ],
   },
 };
