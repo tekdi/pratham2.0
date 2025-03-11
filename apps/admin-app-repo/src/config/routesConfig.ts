8e3ef0d2f0672d65bc3de460709892a43fbe47cf
ttpl-rt-217 <shreyas_s@techjoomla.com>
2025-03-11 14:01:23 +0530

    Issue task PS-4172 :SCP Admin- Team leader, facilitator, learner - List, form and SDBV views changes

--- apps/admin-app-repo/src/config/routesConfig.ts
+++ apps/admin-app-repo/src/config/routesConfig.ts
@@ -10,13 +10,13 @@ export const PUBLIC_ROUTES = [
 ];
 export const ROLE_BASED_ROUTES = {
   [TenantName.SECOND_CHANCE_PROGRAM]: {
-    [Role.ADMIN]: ['/test-route/[identifier]'],
-    [Role.CENTRAL_ADMIN]: ['/test-route/[identifier]'],
+    [Role.ADMIN]: ['/center', '/team-leader' , '/faciliator'  , '/learners' , '/state', '/district', '/block', '/village' ],
+    [Role.CENTRAL_ADMIN]: ['/team-leader', '/faciliator', '/learners' , '/state', '/district', '/block' , '/village'],
     [Role.CCTA]: ['/test-route/[identifier]'],
     [Role.SCTA]: ['/test-route/[identifier]'],
   },
   [TenantName.YOUTHNET]: {
-    [Role.ADMIN]: ['/test-route/[identifier]'],
+    [Role.ADMIN]: ['/mentor', '/mentor-leader', '/certificate-issuance', '/support-request' ],
     [Role.CENTRAL_ADMIN]: ['/test-route/[identifier]'],
     [Role.CCTA]: ['/test-route/[identifier]'],
     [Role.SCTA]: ['/test-route/[identifier]'],
