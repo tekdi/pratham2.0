4a042b764d34e3d2dd7db6560b5f459459e4cdaf
Rushi G <rushi_g@tekditechnologies.com>
2025-03-27 11:19:03 +0530

    rjsf fix

--- apps/admin-app-repo/src/constant/Forms/TeamLeaderSearch.js
+++ apps/admin-app-repo/src/constant/Forms/TeamLeaderSearch.js
@@ -60,7 +60,7 @@ export const TeamLeaderSearchSchema = {
           value: 'value',
         },
         callType: userRole !== Role.CENTRAL_ADMIN ? 'initial' : 'dependent',
-        dependent: 'state',
+        ...(!stateId ? { dependent: 'state' } : {}),
       },
       //for multiselect
       uniqueItems: true,
