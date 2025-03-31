4c537856748f639a075b678e87037ce3b59bd7b6
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2025-03-31 16:00:43 +0530

    Modified Role name and Role Id and fixed center list dropdown inconsistency

--- apps/admin-app-repo/src/components/layouts/header/Profile.tsx
+++ apps/admin-app-repo/src/components/layouts/header/Profile.tsx
@@ -1,28 +1,36 @@
-import CommonUserModal from "@/components/CommonUserModal";
-import { FormContextType, Role, TelemetryEventType } from "@/utils/app.constant";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CommonUserModal from '@/components/CommonUserModal';
+import {
+  FormContextType,
+  Role,
+  TelemetryEventType,
+} from '@/utils/app.constant';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 // import FeatherIcon from "feather-icons-react";
-import { useTranslation } from "next-i18next";
-import React, { useEffect } from "react";
+import { useTranslation } from 'next-i18next';
+import React, { useEffect } from 'react';
 
-import { getFormRead } from "@/services/CreateUserService";
-import { getUserDetailsInfo } from "@/services/UserList";
-import { firstLetterInUpperCase, getInitials, getUserFullName } from "@/utils/Helper";
-import { telemetryFactory } from "@/utils/telemetry";
-import useSubmittedButtonStore from "@/utils/useSharedState";
+import { getFormRead } from '@/services/CreateUserService';
+import { getUserDetailsInfo } from '@/services/UserList';
+import {
+  firstLetterInUpperCase,
+  getInitials,
+  getUserFullName,
+} from '@/utils/Helper';
+import { telemetryFactory } from '@/utils/telemetry';
+import useSubmittedButtonStore from '@/utils/useSharedState';
 import EditIcon from '@mui/icons-material/Edit';
-import LogoutIcon from "@mui/icons-material/Logout";
-import MailIcon from "@mui/icons-material/Mail";
-import PhoneIcon from "@mui/icons-material/Phone";
-import { Box, Button, Divider, Menu, Typography } from "@mui/material";
-import { useRouter } from "next/router";
+import LogoutIcon from '@mui/icons-material/Logout';
+import MailIcon from '@mui/icons-material/Mail';
+import PhoneIcon from '@mui/icons-material/Phone';
+import { Box, Button, Divider, Menu, Typography } from '@mui/material';
+import { useRouter } from 'next/router';
 
 const Profile = () => {
   const [anchorEl4, setAnchorEl4] = React.useState<null | HTMLElement>(null);
   const [profileClick, setProfileClick] = React.useState<boolean>(false);
   const [openEditModal, setOpenEditModal] = React.useState(false);
-  const [userName, setUserName] = React.useState<string | null>("");
-  const [userId, setUserId] = React.useState<string>("");
+  const [userName, setUserName] = React.useState<string | null>('');
+  const [userId, setUserId] = React.useState<string>('');
   const [adminInfo, setAdminInfo] = React.useState<any>();
 
   const [formdata, setFormData] = React.useState<any>();
@@ -43,8 +51,7 @@ const Profile = () => {
     setProfileClick(true);
     const windowUrl = window.location.pathname;
     const cleanedUrl = windowUrl.replace(/^\//, '');
-    const env = cleanedUrl.split("/")[0];
-
+    const env = cleanedUrl.split('/')[0];
 
     const telemetryInteract = {
       context: {
@@ -66,10 +73,10 @@ const Profile = () => {
   };
 
   const handleLogout = () => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      localStorage.removeItem("token");
+    if (typeof window !== 'undefined' && window.localStorage) {
+      localStorage.removeItem('token');
     }
-    router.push("/logout");
+    router.push('/logout');
   };
 
   const mapFields = (formFields: any, response: any) => {
@@ -81,28 +88,24 @@ const Profile = () => {
       );
 
       const getValue = (data: any, field: any) => {
-
         if (item.default) {
           return item.default;
         }
         if (item?.isMultiSelect) {
           if (data[item.name] && item?.maxSelections > 1) {
             return [field?.value];
-          } else if (item?.type === "checkbox") {
-
-
-            return String(field?.value).split(",");
+          } else if (item?.type === 'checkbox') {
+            return String(field?.value).split(',');
           } else {
             return field?.value?.toLowerCase();
           }
         } else {
-          if (item?.type === "numeric") {
+          if (item?.type === 'numeric') {
             return parseInt(String(field?.value));
-          } else if (item?.type === "text") {
+          } else if (item?.type === 'text') {
             return String(field?.value);
           } else {
-            if (field?.value === "FEMALE" || field?.value === "MALE") {
-
+            if (field?.value === 'FEMALE' || field?.value === 'MALE') {
               return field?.value?.toLowerCase();
             }
 
@@ -115,20 +118,16 @@ const Profile = () => {
         if (item?.isMultiSelect) {
           if (userData[item.name] && item?.maxSelections > 1) {
             initialFormData[item.name] = [userData[item.name]];
-          } else if (item?.type === "checkbox") {
-
-
-            initialFormData[item.name] = String(userData[item.name]).split(",");
+          } else if (item?.type === 'checkbox') {
+            initialFormData[item.name] = String(userData[item.name]).split(',');
           } else {
             initialFormData[item.name] = userData[item.name];
           }
-        } else if (item?.type === "numeric") {
-
+        } else if (item?.type === 'numeric') {
           initialFormData[item.name] = Number(userData[item.name]);
-        } else if (item?.type === "text" && userData[item.name]) {
+        } else if (item?.type === 'text' && userData[item.name]) {
           initialFormData[item.name] = String(userData[item.name]);
         } else {
-
           if (userData[item.name]) {
             initialFormData[item.name] = userData[item.name];
           }
@@ -141,7 +140,6 @@ const Profile = () => {
         }
       }
     });
-
 
     return initialFormData;
   };
@@ -159,28 +157,26 @@ const Profile = () => {
 
       let formFields;
       if (Role.STUDENT === adminInformation?.role) {
-        formFields = await getFormRead("USERS", "STUDENT");
+        formFields = await getFormRead('USERS', 'LEARNER');
         setFormData(mapFields(formFields, response));
       } else if (Role.TEACHER === adminInformation?.role) {
-        formFields = await getFormRead("USERS", "TEACHER");
+        formFields = await getFormRead('USERS', 'INSTRUCTOR');
         setFormData(mapFields(formFields, response));
       } else if (Role.TEAM_LEADER === adminInformation?.role) {
-        formFields = await getFormRead("USERS", "TEAM LEADER");
+        formFields = await getFormRead('USERS', 'LEAD');
         setFormData(mapFields(formFields, response));
       } else {
-        formFields = await getFormRead("USERS", "ADMIN");
+        formFields = await getFormRead('USERS', 'ADMIN');
         setFormData(mapFields(formFields, response));
       }
       handleOpenEditModal();
-
-
     } catch (error) {
       console.log(error);
     }
   };
 
   const getUserName = () => {
-      setUserName(getUserFullName());
+    setUserName(getUserFullName());
   };
 
   const handleCloseAddLearnerModal = () => {
@@ -195,9 +191,10 @@ const Profile = () => {
   }, [formdata]);
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      const admin = localStorage.getItem("adminInfo");
-      if (admin && admin !== "undefined") setAdminInfo(JSON?.parse(admin) || {});
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const admin = localStorage.getItem('adminInfo');
+      if (admin && admin !== 'undefined')
+        setAdminInfo(JSON?.parse(admin) || {});
     }
   }, []);
 
@@ -222,7 +219,6 @@ const Profile = () => {
     router.push('/edit-password'); // Then navigate to the edit password page
   };
 
-
   return (
     <>
       <Button
@@ -232,24 +228,24 @@ const Profile = () => {
         aria-haspopup="true"
         onClick={handleClick4}
         sx={{
-          border: "none",
-          paddingLeft: "0px !important",
-          paddingRight: "0px !important",
+          border: 'none',
+          paddingLeft: '0px !important',
+          paddingRight: '0px !important',
           '@media (max-width: 600px)': {
-            minWidth: "0px !important",
-          }
+            minWidth: '0px !important',
+          },
         }}
       >
         <Box display="flex" alignItems="center" color="white">
           <AccountCircleIcon />
           <Box
             sx={{
-              display: "flex",
-              alignItems: "center",
+              display: 'flex',
+              alignItems: 'center',
               // ml: 1,
               '@media (max-width: 600px)': {
-                display: 'none'
-              }
+                display: 'none',
+              },
             }}
           >
             <Typography
@@ -257,16 +253,16 @@ const Profile = () => {
               fontWeight="400"
               sx={{
                 ml: 1,
-                maxWidth: "200px",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                fontSize: "16px",
-                whiteSpace: "nowrap",
+                maxWidth: '200px',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                fontSize: '16px',
+                whiteSpace: 'nowrap',
               }}
             >
-              {t("COMMON.HI", { name: firstLetterInUpperCase(userName ?? "") })}
+              {t('COMMON.HI', { name: firstLetterInUpperCase(userName ?? '') })}
             </Typography>
-{/* 
+            {/* 
             <FeatherIcon icon="chevron-down" size="20" /> */}
           </Box>
         </Box>
@@ -277,20 +273,20 @@ const Profile = () => {
         open={Boolean(anchorEl4)}
         onClose={handleClose4}
         sx={{
-          paddingTop: "0px",
+          paddingTop: '0px',
           //backgroundColor: "#F8EFE7",
         }}
         PaperProps={{
           sx: {
             // width: "500px",
-            minWidth: "320px",
-            borderRadius: "12px",
+            minWidth: '320px',
+            borderRadius: '12px',
           },
         }}
         MenuListProps={{
           sx: {
-            paddingTop: "50px !important",
-            paddingBottom: "0px !important",
+            paddingTop: '50px !important',
+            paddingBottom: '0px !important',
           },
         }}
       >
@@ -298,48 +294,45 @@ const Profile = () => {
           sx={{ backgroundColor: "#F8EFE7", height: "56px", width: "100%" }}
         ></Box> */}
         <Box
-          sx={{ display: "flex", justifyContent: "center", marginTop: "-25px" }}
+          sx={{ display: 'flex', justifyContent: 'center', marginTop: '-25px' }}
         >
           <Box
             sx={{
-              backgroundColor: "#78590C",
-              width: "50px",
-              height: "50px",
-              borderRadius: "50%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              backgroundColor: '#78590C',
+              width: '50px',
+              height: '50px',
+              borderRadius: '50%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
             <Typography
               variant="h6"
               color="white"
-              sx={{ fontWeight: "bold", fontSize: "18px" }}
+              sx={{ fontWeight: 'bold', fontSize: '18px' }}
             >
               {getInitials(userName)}
-            
             </Typography>
-
-
           </Box>
         </Box>
 
         <Box
           sx={{
-            position: "relative",
+            position: 'relative',
             // marginTop:"10px",
             // borderRadius: "10px",
-            backgroundColor: "white",
+            backgroundColor: 'white',
           }}
         >
           <Typography
             variant="h5"
             sx={{
-              marginBottom: "15px",
-              marginTop: "10px",
-              textAlign: "center",
-              px: "20px",
-              fontSize: "16px",
+              marginBottom: '15px',
+              marginTop: '10px',
+              textAlign: 'center',
+              px: '20px',
+              fontSize: '16px',
             }}
           >
             {userName}
@@ -347,57 +340,69 @@ const Profile = () => {
           <Typography
             variant="subtitle1"
             sx={{
-              marginBottom: "20px",
-              textAlign: "center",
-              px: "20px",
-              color: "#7C766F",
-              fontSize: "14px",
+              marginBottom: '20px',
+              textAlign: 'center',
+              px: '20px',
+              color: '#7C766F',
+              fontSize: '14px',
             }}
           >
             {adminInfo?.role}
           </Typography>
           <Box
             sx={{
-              display: "flex",
-              alignItems: "center",
-              marginBottom: "10px",
-              px: "20px",
+              display: 'flex',
+              alignItems: 'center',
+              marginBottom: '10px',
+              px: '20px',
             }}
           >
-            <PhoneIcon sx={{ marginRight: "10px" }} />
-            <Typography variant="body1" sx={{ fontSize: "14px" }}>
+            <PhoneIcon sx={{ marginRight: '10px' }} />
+            <Typography variant="body1" sx={{ fontSize: '14px' }}>
               {adminInfo?.mobile}
             </Typography>
           </Box>
           <Box
             sx={{
-              display: "flex",
-              alignItems: "center",
-              marginBottom: "20px",
-              px: "20px",
+              display: 'flex',
+              alignItems: 'center',
+              marginBottom: '20px',
+              px: '20px',
             }}
           >
-            <MailIcon sx={{ marginRight: "10px" }} />
-            <Typography variant="body1" sx={{ fontSize: "14px" }}>
+            <MailIcon sx={{ marginRight: '10px' }} />
+            <Typography variant="body1" sx={{ fontSize: '14px' }}>
               {adminInfo?.email}
             </Typography>
           </Box>
 
-          <Divider sx={{ color: "#D0C5B4" }} />
-          <Box sx={{ px: "20px", display: "flex", gap: "10px", justifyContent: "space-between", alignItems: "center", '@media (max-width: 434px)': { flexDirection: 'column', justifyContent: 'center', alignItems: 'center' } }}>
+          <Divider sx={{ color: '#D0C5B4' }} />
+          <Box
+            sx={{
+              px: '20px',
+              display: 'flex',
+              gap: '10px',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              '@media (max-width: 434px)': {
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              },
+            }}
+          >
             <Button
               fullWidth
               variant="outlined"
               color="primary"
               onClick={handleEditPassword}
               sx={{
-                fontSize: "16px",
-                backgroundColor: "white",
-                border: "0.6px solid #1E1B16",
-                my: "20px",
-                width: "408px",
-                '@media (max-width: 434px)': { width: '100%' }
-
+                fontSize: '16px',
+                backgroundColor: 'white',
+                border: '0.6px solid #1E1B16',
+                my: '20px',
+                width: '408px',
+                '@media (max-width: 434px)': { width: '100%' },
               }}
               endIcon={<EditIcon />}
             >
@@ -409,17 +414,16 @@ const Profile = () => {
               color="primary"
               onClick={handleLogout}
               sx={{
-                fontSize: "16px",
-                backgroundColor: "#FDBE16",
-                border: "0.6px solid #1E1B16",
-                my: "20px",
-                '@media (max-width: 434px)': { my: '0px', mb: '20px' }
+                fontSize: '16px',
+                backgroundColor: '#FDBE16',
+                border: '0.6px solid #1E1B16',
+                my: '20px',
+                '@media (max-width: 434px)': { my: '0px', mb: '20px' },
               }}
               endIcon={<LogoutIcon />}
             >
-              {t("COMMON.LOGOUT")}
+              {t('COMMON.LOGOUT')}
             </Button>
-
           </Box>
         </Box>
       </Menu>
