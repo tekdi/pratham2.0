20d7adf2bebfef1da6614791a1b55afe450f46d0
ttpl-rt-217 <shreyas_s@techjoomla.com>
2025-04-22 12:09:16 +0530

    Issue feat PS-4451:L2 interest-based courses workflow UI Implement (after login)

--- apps/learner-web-app/src/app/content/LevelUp.tsx
+++ apps/learner-web-app/src/app/content/LevelUp.tsx
@@ -1,5 +1,11 @@
-import { Box, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from '@mui/material';
+import React from 'react';
 
 interface LevelUpProps {
   handleTopicChange: (event: SelectChangeEvent) => void;
@@ -7,44 +13,59 @@ interface LevelUpProps {
 }
 
 const LevelUp: React.FC<LevelUpProps> = ({
-    handleTopicChange,
-    selectedTopic
+  handleTopicChange,
+  selectedTopic,
 }) => {
-   
   return (
     <div>
-          <Typography variant="h5" component="h2" fontWeight="medium" mb={3} id="modal-title">
-              Great! We're Excited to Help You Level Up!
-          </Typography>
+      <Typography
+        sx={{
+          color: '#1F1B13',
+          textAlign: 'center',
+        }}
+        variant="h1"
+        mb={3}
+        id="modal-title"
+      >
+        Great! We're Excited to Help You Level Up!
+      </Typography>
 
-          <Typography variant="subtitle1" mb={2}>
-              Which topic are you most interested in?
-          </Typography>
+      <Typography
+        variant="h2"
+        sx={{
+          color: '#1F1B13',
+          textAlign: 'center',
+        }}
+        mb={2}
+      >
+        Which topic are you most interested in?
+      </Typography>
 
-          <Box sx={{ mb: 2 }}>
-              <Select
-                  value={selectedTopic}
-                  onChange={handleTopicChange}
-                  displayEmpty
-                  fullWidth
-                  sx={{ textAlign: 'left' }}
-              >
-                  <MenuItem disabled value="">
-                      <em>Topics</em>
-                  </MenuItem>
-                  <MenuItem value="plumbing">Plumbing</MenuItem>
-                  <MenuItem value="electrical">Electrical</MenuItem>
-                  <MenuItem value="carpentry">Carpentry</MenuItem>
-                  <MenuItem value="masonry">Masonry</MenuItem>
-                  <MenuItem value="painting">Painting</MenuItem>
-              </Select>
-          </Box>
+      <Box sx={{ mb: 2 }}>
+        <Select
+          value={selectedTopic}
+          onChange={handleTopicChange}
+          displayEmpty
+          fullWidth
+          sx={{ textAlign: 'left' }}
+        >
+          <MenuItem disabled value="">
+            <em>Topics</em>
+          </MenuItem>
+          <MenuItem value="plumbing">Plumbing</MenuItem>
+          <MenuItem value="electrical">Electrical</MenuItem>
+          <MenuItem value="carpentry">Carpentry</MenuItem>
+          <MenuItem value="masonry">Masonry</MenuItem>
+          <MenuItem value="painting">Painting</MenuItem>
+        </Select>
+      </Box>
 
-          <Typography variant="body2" color="text.secondary" mb={3}>
-              Once you select a course, our expert will contact you with more information!
-          </Typography>
+          <Typography variant="h4" color="#635E57" mb={3}>
+        Once you select a course, our expert will contact you with more
+        information!
+      </Typography>
     </div>
-  )
-}
+  );
+};
 
-export default LevelUp
+export default LevelUp;
--- apps/learner-web-app/src/app/content/ResponseRecorded.tsx
+++ apps/learner-web-app/src/app/content/ResponseRecorded.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import { Box, Typography, Button } from '@mui/material';
 
-interface ResponseRecordedProps {
-}
+interface ResponseRecordedProps {}
 
 const ResponseRecorded: React.FC<ResponseRecordedProps> = () => {
   return (
@@ -13,23 +12,44 @@ const ResponseRecorded: React.FC<ResponseRecordedProps> = () => {
         flexDirection: 'column',
         alignItems: 'center',
         textAlign: 'center',
-        padding: '16px',
       }}
     >
       <Box sx={{ color: '#4CAF50', fontSize: '48px', mb: 2 }}>
         <CheckCircleOutlineIcon fontSize="inherit" />
       </Box>
-      
-      <Typography variant="h5" fontWeight="medium" mb={2}>
+
+      <Typography
+        variant="h1"
+        sx={{
+          color: '#1F1B13',
+          textAlign: 'center',
+        }}
+        mb={2}
+      >
         Your response has been recorded
       </Typography>
-      
-      <Typography variant="body1" mb={1}>
+
+      <Typography
+        variant="body1"
+        mb={1}
+        sx={{
+          color: '#1F1B13',
+          textAlign: 'center',
+        }}
+      >
         Our expert will reach out to you soon
       </Typography>
-      
-      <Typography variant="body2" color="text.secondary" mb={4}>
-        In the meantime, feel free to explore more courses or continue building your skills!
+
+      <Typography
+        variant="body1"
+        sx={{
+          color: '#1F1B13',
+          textAlign: 'center',
+        }}
+        mb={4}
+      >
+        In the meantime, feel free to explore more courses or continue building
+        your skills!
       </Typography>
     </Box>
   );
