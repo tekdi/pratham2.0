15ae2dc9337722b5bbcdd1988827ca77c5f4a31c
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-04-24 15:59:11 +0530

    Issue feat: apply filters for l1 course and Integrate courseId api and  removed hardcdeed doIds

--- apps/learner-web-app/src/utils/API/ProgramService.ts
+++ apps/learner-web-app/src/utils/API/ProgramService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { API_ENDPOINTS } from './EndUrls';
-
+import { post } from '@shared-lib';
 
 export const getTenantInfo = async (): Promise<any> => {
   const apiUrl = API_ENDPOINTS.program;
@@ -12,5 +12,19 @@ export const getTenantInfo = async (): Promise<any> => {
   } catch (error) {
     console.error('Error in fetching tenant info', error);
     throw null;
+  }
+};
+
+export const FetchDoIds = async (userId: any[]): Promise<any> => {
+  const apiUrl: string = API_ENDPOINTS.fetchCourseId;
+
+  try {
+    const response = await post(apiUrl, {
+      userId: userId,
+    });
+    return response?.data;
+  } catch (error) {
+    console.error('error in login', error);
+    throw error;
   }
 };
