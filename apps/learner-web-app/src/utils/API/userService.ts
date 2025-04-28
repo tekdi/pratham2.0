9cdc8516f4558ce17758dc1323ca8b1c05c16b95
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-04-28 15:44:00 +0530

    Issue feat: Profile management flow

--- apps/learner-web-app/src/utils/API/userService.ts
+++ apps/learner-web-app/src/utils/API/userService.ts
@@ -1,6 +1,13 @@
+import { fetchForm } from '@shared-lib-v2/DynamicForm/components/DynamicFormCallback';
 import { API_ENDPOINTS } from './EndUrls';
-import { post } from './RestClient';
+import { post, get, patch } from './RestClient';
+import { FormContext } from '@shared-lib-v2/DynamicForm/components/DynamicFormConstant';
+import { getMissingFields } from '../helper';
+export interface UserDetailParam {
+  userData?: object;
 
+  customFields?: any;
+}
 interface UserCheckParams {
   username?: string;
   mobile?: string;
@@ -30,5 +37,69 @@ export const userCheck = async ({
   } catch (error) {
     console.error('error in login', error);
     throw error;
+  }
+};
+
+export const profileComplitionCheck = async (): Promise<any> => {
+  let userId = localStorage.getItem('userId');
+  try {
+    if (userId) {
+      let apiUrl: string = API_ENDPOINTS.userRead(userId, true);
+
+      const response = await get(apiUrl);
+      console.log('response', response);
+      let userData = response?.data?.result?.userData;
+      const responseForm: any = await fetchForm([
+        {
+          fetchUrl: `${process.env.NEXT_PUBLIC_MIDDLEWARE_URL}/form/read?context=${FormContext.learner.context}&contextType=${FormContext.learner.contextType}`,
+          header: {},
+        },
+        {
+          fetchUrl: `${process.env.NEXT_PUBLIC_MIDDLEWARE_URL}/form/read?context=${FormContext.learner.context}&contextType=${FormContext.learner.contextType}`,
+          header: {
+            tenantid: localStorage.getItem('tenantId'),
+          },
+        },
+      ]);
+      console.log('responseForm', responseForm?.schema);
+
+      const result = getMissingFields(responseForm?.schema, userData);
+      console.log('result', result);
+      const isPropertiesEmpty = Object.keys(result.properties).length === 0;
+      return isPropertiesEmpty;
+    }
+  } catch (error) {
+    console.error('error in login', error);
+    throw error;
+  }
+};
+
+export const updateUser = async (
+  userId: string,
+  { userData, customFields }: UserDetailParam
+): Promise<any> => {
+  const apiUrl: string = API_ENDPOINTS.userUpdate(userId);
+
+  try {
+    const response = await patch(apiUrl, { userData, customFields });
+    return response;
+  } catch (error) {
+    console.error('error in fetching user details', error);
+    return error;
+  }
+};
+export const getUserDetails = async (
+  userId: string | string[],
+  fieldValue: boolean
+): Promise<any> => {
+  let apiUrl: string = API_ENDPOINTS.userRead(userId, fieldValue);
+  // apiUrl = fieldValue ? `${apiUrl}?fieldvalue=true` : apiUrl;
+
+  try {
+    const response = await get(apiUrl);
+    return response?.data;
+  } catch (error) {
+    console.error('error in fetching user details', error);
+    return error;
   }
 };
