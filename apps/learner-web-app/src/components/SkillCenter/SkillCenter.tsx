4a966536e8754eea97671d07d8c02f11923449c1
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-06-06 18:42:34 +0530

    Issue feat: Resolve rendering issue

--- apps/learner-web-app/src/components/SkillCenter/SkillCenter.tsx
+++ apps/learner-web-app/src/components/SkillCenter/SkillCenter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   Box,
   Typography,
@@ -9,23 +9,27 @@ import {
   Card,
   CardContent,
   Chip,
-  Container,
-  styled,
   CircularProgress,
+  styled,
 } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useRouter } from 'next/navigation';
+
+import DynamicForm from '@shared-lib-v2/DynamicForm/components/DynamicForm';
+import {
+  fetchForm,
+  searchListData,
+} from '@shared-lib-v2/DynamicForm/components/DynamicFormCallback';
 import {
   searchCohort,
   CohortDetails,
-  getUserCohortsRead,
 } from '@learner/utils/API/CohortService';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { useRouter } from 'next/navigation';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import DynamicForm from '@shared-lib-v2/DynamicForm/components/DynamicForm';
-import { fetchForm, searchListData } from '@shared-lib-v2/DynamicForm/components/DynamicFormCallback';
-import { CohortSearchSchema, CohortSearchUISchema } from '../CohortSearch';
-import { FormContext } from '@shared-lib-v2/DynamicForm/components/DynamicFormConstant';
+import {
+  CohortSearchSchema,
+  CohortSearchUISchema,
+} from '../CohortSearch';
 
 interface Center {
   name: string;
@@ -39,7 +43,6 @@ interface Center {
     label: string;
     selectedValues: string[];
   }[];
-  
 }
 
 interface SkillCenterProps {
@@ -48,11 +51,11 @@ interface SkillCenterProps {
   viewAll?: boolean;
   Limit?: number;
   visibleCenters?: any;
-  setVisibleCenters?: any
+  setVisibleCenters?: any;
   hideFilter?: boolean;
 }
 
-const ImageContainer = styled(Box)(({ theme }) => ({
+const ImageContainer = styled(Box)({
   position: 'relative',
   borderRadius: '8px',
   overflow: 'hidden',
@@ -64,9 +67,9 @@ const ImageContainer = styled(Box)(({ theme }) => ({
     borderRadius: '8px',
     objectFit: 'cover',
   },
-}));
+});
 
-const ImageOverlay = styled(Box)(({ theme }) => ({
+const ImageOverlay = styled(Box)({
   position: 'absolute',
   bottom: 0,
   right: 0,
@@ -80,29 +83,16 @@ const ImageOverlay = styled(Box)(({ theme }) => ({
   borderRadius: '8px',
   fontSize: '16px',
   fontWeight: 500,
-}));
+});
 
-const getCustomFieldValue = (
-  cohort: CohortDetails,
-  fieldLabel: string
-): string | null => {
-  console.log(fieldLabel, 'fieldLabel');
-  console.log(cohort, 'cohort');
-
-  const field = cohort.customFields.find((f) => f.label === fieldLabel);
-  console.log(field, 'field');
-
-  if (field && field.selectedValues.length > 0) {
-    return field.selectedValues[0] as any;
-  }
-  return null;
+const getCustomFieldValue = (cohort: CohortDetails, label: string) => {
+  const field = cohort.customFields.find(f => f.label === label);
+  return field?.selectedValues?.[0] || "";
 };
+
 const getIndustryValues = (cohort: CohortDetails): string[] => {
-  const industryField = cohort.customFields.find((f) => f.label === 'INDUSTRY');
-  if (industryField) {
-    return industryField.selectedValues.map((v) => v.label || v.value);
-  }
-  return [];
+  const industryField = cohort.customFields.find(f => f.label === 'INDUSTRY');
+  return industryField ? industryField.selectedValues.map(v => v.label || v.value) : [];
 };
 
 const SkillCenter = ({
@@ -112,160 +102,101 @@ const SkillCenter = ({
   Limit,
   visibleCenters,
   setVisibleCenters,
-  hideFilter=true
+  hideFilter = true,
 }: SkillCenterProps) => {
   const router = useRouter();
   const [centers, setCenters] = useState<Center[]>([]);
   const [loading, setLoading] = useState(true);
-  const [offset, setOffset] = useState(0);
-  const [hasMore, setHasMore] = useState(true);
-   const [schema, setSchema] = useState(CohortSearchSchema);
-  const [uiSchema, setUiSchema] = useState(CohortSearchUISchema);
-    const [prefilledFormData, setPrefilledFormData] = useState<any>(null);
-   const [isLoading, setIsLoading] = useState(false);
-  
-  const [addSchema, setAddSchema] = useState(null);
-  const [addUiSchema, setAddUiSchema] = useState(null);
-  const [prefilledAddFormData, setPrefilledAddFormData] = useState({});
-  const [pageLimit, setPageLimit] = useState<number>(10);
-  const [pageOffset, setPageOffset] = useState<number>(0);
- 
+  const [schema] = useState(CohortSearchSchema);
+  const [uiSchema] = useState(CohortSearchUISchema);
+  const [prefilledFormData, setPrefilledFormData] = useState<any>(null);
   const [response, setResponse] = useState<any>(null);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [openModal, setOpenModal] = React.useState<boolean>(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [editableUserId, setEditableUserId] = useState('');
-    // const [visibleCenters, setVisibleCenters] = useState<any>([]);
-
-  const [cohortId, setCohortId] = useState('');
-  const [tenantId, setTenantId] = useState('');
-  const [open, setOpen] = useState(false);
-  const [firstName, setFirstName] = useState('');
-  const [totalCount, setTotalCount] = useState(0);
-  const [totalCountBatch, setTotalCountBatch] = useState(0);
-    const searchStoreKey = 'centers';
-
- const initialFormDataSearch =
-  typeof window !== 'undefined' && typeof localStorage !== 'undefined'
-    ? localStorage.getItem(searchStoreKey) && localStorage.getItem(searchStoreKey) !== '{}'
-      ? JSON.parse(localStorage.getItem(searchStoreKey) || '')
+  const isFetched = useRef(false);
+ const [pageLimit, setPageLimit] = useState<number>(10);
+  const [pageOffset, setPageOffset] = useState<number>(0);
+  const searchStoreKey = 'centers';
+  let totalcount=100;
+  const initialFormDataSearch =
+    typeof window !== 'undefined' && localStorage.getItem(searchStoreKey)
+      ? JSON.parse(localStorage.getItem(searchStoreKey) || '{}')
       : localStorage.getItem('stateId')
       ? { state: [localStorage.getItem('stateId')] }
-      : {}
-    : {};
+      : {};
 
-useEffect(() => {
-    // Fetch form schema from API and set it in state.
-      
+  useEffect(() => {
     setPrefilledFormData(initialFormDataSearch);
-  }, [initialFormDataSearch]);
-  console.log("predfilledformdata", prefilledFormData)
-  const limit = 10;
+  }, []);
 
-  const fetchCenters = async (currentOffset: number) => {
-    try {
-     
-      if (response?.result.result.results?.cohortDetails) {
-        const apiCenters: Center[] = response.result.result.results.cohortDetails.map((cohort: CohortDetails) => ({
-          name: cohort.name,
-          category: getIndustryValues(cohort)[0] || 'General',
-          address: getCustomFieldValue(cohort, 'ADDRESS') || 'Address not available',
-          distance: '0 km',
-          mapsUrl: getCustomFieldValue(cohort, 'GOOGLE_MAP_LINK') || '#',
-          images: cohort.image || ['/images/default.png'],
-          moreImages: cohort.image?.length > 3 ? cohort.image.length - 3 : 0,
-        }));
+  const fetchCenters = async () => {
+    if (!response?.result?.result?.results?.cohortDetails) return;
 
-        if (currentOffset === 0) {
-          setCenters(apiCenters);
-            const visibleCenters = viewAll ? apiCenters : apiCenters.slice(0, Limit);
-            setVisibleCenters(visibleCenters)
+    const apiCenters: Center[] = response.result.result.results.cohortDetails.map((cohort: CohortDetails) => ({
+      name: cohort.name,
+      category: getIndustryValues(cohort)[0] || 'General',
+      address: getCustomFieldValue(cohort, 'ADDRESS') || 'Address not available',
+      distance: '0 km',
+      mapsUrl: getCustomFieldValue(cohort, 'GOOGLE_MAP_LINK') || '#',
+      images: cohort.image || ['/images/default.png'],
+      moreImages: cohort.image?.length > 3 ? cohort.image.length - 3 : 0,
+    }));
 
-        } else {
-          const r: any=((prev: any) => [...prev, ...apiCenters])
-                      const visibleCenters = viewAll ? r : r.slice(0, Limit);
-                      setVisibleCenters(visibleCenters)
+    setCenters(apiCenters);
+setCenters((prev) => [...prev, ...apiCenters]); // ✅ append
+  setVisibleCenters((prev: any) =>
+    viewAll ? [...prev, ...apiCenters] : [...prev, ...apiCenters].slice(0, Limit)
+  );    setLoading(false);
+  };
 
-          setCenters(prev => [...prev, ...apiCenters]);
-        }
-
-        setHasMore(apiCenters.length === limit);
-      }
-      else{
-              setVisibleCenters([])
-
-      }
-    } catch (error) {
-      setVisibleCenters([])
-      console.error('Failed to fetch centers:', error);
-    } finally {
+  useEffect(() => {
+    if (response?.result?.result?.results?.cohortDetails?.length > 0 && !isFetched.current) {
+      isFetched.current = true;
+      fetchCenters();
+    } else if (!response || !response.result?.result?.results?.cohortDetails?.length) {
+      setCenters([]);
+      setVisibleCenters([]);
       setLoading(false);
     }
-  };
-
- useEffect(() => {
-  if( JSON.stringify(response)) {
-  fetchCenters(0);
-  }
-}, [JSON.stringify(response)]);
-
-
-  const handleLoadMore = () => {
-    const newOffset = offset + limit;
-    setOffset(newOffset);
-    fetchCenters(newOffset);
-  };
-
- 
+    else{
+ setCenters([]);
+      setVisibleCenters([]);
+      setLoading(false);    }
     
-      console.log("Youth@16",visibleCenters)
-  if (loading && centers.length === 0) {
-    return (
-      <Box sx={{ p: 3 }}>
-        <Typography>Loading centers...</Typography>
-      </Box>
-    );
-  }
- 
+  }, [response]);
 
-  const updatedUiSchema = {
-    ...uiSchema,
-    'ui:submitButtonOptions': {
-      norender: true, // Hide submit button if isHide is true
-    },
+  const searchData = async (formData: any) => {
+    if (!formData) return;
+    formData = Object.fromEntries(Object.entries(formData).filter(([_, v]) => !Array.isArray(v) || v.length > 0));
+    const staticFilter = { type: 'COHORT' };
+    const sort = ['name', formData.sortBy || 'asc'];
+
+    await searchListData(
+      formData,
+      0,
+      staticFilter,
+      10,
+      () => {},
+      () => {},
+      (newResponse: any) => setResponse({ ...newResponse }),
+      searchCohort,
+      sort
+    );
   };
-    const SubmitaFunction = async (formData: any) => {
-    // console.log("###### debug issue formData", formData)
-    if (formData && Object.keys(formData).length > 0) {
-      setPrefilledFormData(formData);
-      //set prefilled search data on refresh
-      localStorage.setItem(searchStoreKey, JSON.stringify(formData));
-      await searchData(formData, 0);
-    }
+
+  const SubmitaFunction = async (formData: any) => {
+    setPrefilledFormData(formData);
+    localStorage.setItem(searchStoreKey, JSON.stringify(formData));
+    isFetched.current = false;
+    await searchData(formData);
   };
- const searchData = async (formData: any, newPage: any) => {
-    if (formData) {
-      formData = Object.fromEntries(
-        Object.entries(formData).filter(
-          ([_, value]) => !Array.isArray(value) || value.length > 0
-        )
-      );
-      const staticFilter = { type: 'COHORT' };
-      const { sortBy } = formData;
-      const staticSort = ['name', sortBy || 'asc'];
-      await searchListData(
-        formData,
-        newPage,
-        staticFilter,
-        pageLimit,
-        setPageOffset,
-        setCurrentPage,
-  (newResponse: any) => setResponse({ ...newResponse }), // 👈 Force new reference
-        searchCohort,
-        staticSort
-      );
-    }
-  };
+const handleLoadMore = () => {
+  console.log("pageLimit",pageLimit, pageOffset)
+  const newOffset = pageOffset + 1;
+ setPageOffset(newOffset);
+  // setPageLimit(pageLimit); // keep limit per page
+  isFetched.current = false;
+  searchData(prefilledFormData);
+};
+
   return (
     <Box sx={{ p: 3 }}>
       <Box
@@ -325,21 +256,20 @@ useEffect(() => {
         )}
       </Box>
 
-          { !hideFilter &&  schema &&
-          uiSchema && (
-            <DynamicForm
-              schema={schema}
-              uiSchema={updatedUiSchema}
-              SubmitaFunction={SubmitaFunction}
-              isCallSubmitInHandle={true}
-              prefilledFormData={prefilledFormData}
-            />
+      {!hideFilter && (
+        <DynamicForm
+          schema={schema}
+          uiSchema={{ ...uiSchema, 'ui:submitButtonOptions': { norender: true } }}
+          SubmitaFunction={SubmitaFunction}
+          isCallSubmitInHandle={true}
+          prefilledFormData={prefilledFormData}
+        />
           )
         }
      <Grid container spacing={3} marginTop={"80px"}>
   {(viewAll ? visibleCenters : visibleCenters?.slice(0, 3))?.map(
     (center: any, idx: any) => (
-      <Grid item xs={12} sm={6} md={4} key={idx}>
+          <Grid item xs={12} sm={6} md={4} key={idx}>
         <Card
           sx={{
             height: '100%',
@@ -352,19 +282,19 @@ useEffect(() => {
             },
           }}
         >
-          <CardContent sx={{ p: 0 }}>
-            <Box sx={{ display: 'flex', gap: 1, mb: 2 }}>
-              {center.images.slice(0, 3).map((img: any, i: any) => (
-                <ImageContainer key={i}>
-                  <img src={img} alt={`${center.name} view ${i + 1}`} />
-                  {i === 2 && center.moreImages > 0 && (
-                    <ImageOverlay>+{center.moreImages}</ImageOverlay>
-                  )}
-                </ImageContainer>
-              ))}
-            </Box>
+              <CardContent sx={{ p: 0 }}>
+                <Box sx={{ display: 'flex', gap: 1, mb: 2 }}>
+                  {center.images.slice(0, 3).map((img: string, i: number) => (
+                    <ImageContainer key={i}>
+                      <img src={img} alt={`${center.name} view ${i + 1}`} />
+                      {i === 2 && center.moreImages > 0 && (
+                        <ImageOverlay>+{center.moreImages}</ImageOverlay>
+                      )}
+                    </ImageContainer>
+                  ))}
+                </Box>
 
-            <Box sx={{ px: 2, pb: 2 }}>
+                <Box sx={{ px: 2, pb: 2 }}>
               <Box
                 sx={{
                   display: 'flex',
@@ -396,7 +326,7 @@ useEffect(() => {
                     height: '24px',
                   }}
                 />
-              </Box>
+                  </Box>
 
               <Typography
                 variant="body2"
@@ -427,35 +357,16 @@ useEffect(() => {
                   },
                 }}
               >
-                Open on Maps <LocationOnIcon sx={{ fontSize: 18 }} />
-              </Link>
-            </Box>
-          </CardContent>
-        </Card>
+                    Open on Maps <LocationOnIcon sx={{ fontSize: 18 }} />
+                  </Link>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-    )
-  )}
-</Grid>
-
-
-      {!loading && visibleCenters?.length === 0 && (
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '200px',
-            width: '100%',
-          }}
-        >
-          <Typography variant="h6" sx={{ color: '#635E57', fontSize: '16px' }}>
-            No data found
-          </Typography>
-        </Box>
-      )}
-
-      {viewAll && hasMore && offset > 10 && (
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+{/* {visibleCenters?.length < 200 && (
+   <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
           <Box
             onClick={handleLoadMore}
             sx={{
@@ -475,6 +386,12 @@ useEffect(() => {
             {loading ? 'Loading...' : 'Load More'}
             {loading && <CircularProgress size={20} color="inherit" />}
           </Box>
+        </Box>
+)} */}
+
+      {!loading && visibleCenters?.length === 0 && (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
+          <Typography variant="h4" sx={{ color: '#635E57' }}>No data found</Typography>
         </Box>
       )}
     </Box>
