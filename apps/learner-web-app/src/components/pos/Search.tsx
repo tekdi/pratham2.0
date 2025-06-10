33f8b82abee5f4f0cf9f752b1b8e220d99455961
sagar <sagarkoshti1990@gmila.com>
2025-06-10 19:52:05 +0530

    fix: issue

--- apps/learner-web-app/src/components/pos/Search.tsx
+++ apps/learner-web-app/src/components/pos/Search.tsx
@@ -19,6 +19,7 @@ const SearchPage = () => {
   React.useEffect(() => {
     const queryParam = searchParams?.get('q') || '';
     setSearchValue(queryParam);
+    setSubmitText(queryParam);
   }, [searchParams]);
 
   const handleSearch = (value: string) => {
