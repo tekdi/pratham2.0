241af4c9c04843182bdfbaf4a788ba1cbe2a8418
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-06-12 14:54:33 +0530

    update pr

--- apps/learner-web-app/src/components/Layout.tsx
+++ apps/learner-web-app/src/components/Layout.tsx
@@ -110,13 +110,14 @@ const App: React.FC<LayoutProps> = ({ children, ...props }) => {
         );
       } else {
         navLinks.push(
-           {
-          title: t('COMMON.SKILLING_CENTERS'),
-          // icon: <Logout sx={{ width: 28, height: 28 }} />,
-          to: () => handleNavClick(() => router.push('/skill-center')),
-          isActive:currentPage === '/skill-center',
-          customStyle: {},
-        },
+          {
+  title: t('COMMON.SKILLING_CENTERS'),
+  icon: <img src="images/engineering.png" alt="Skill Center" style={{ width: 28, height: 28 }} />,
+  to: () => handleNavClick(() => router.push('/skill-center')),
+  isActive: currentPage === '/skill-center',
+  customStyle: {},
+}
+,
           {
           title: t('LEARNER_APP.COMMON.PROFILE'),
           icon: <AccountCircleOutlined sx={{ width: 28, height: 28 }} />,
