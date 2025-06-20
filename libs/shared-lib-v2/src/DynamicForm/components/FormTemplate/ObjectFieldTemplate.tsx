4365a0dd39a907618579bb5d5565b79567cb49d6
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-06-20 18:42:48 +0530

    Issue feat:Fix build issue

--- libs/shared-lib-v2/src/DynamicForm/components/FormTemplate/ObjectFieldTemplate.tsx
+++ libs/shared-lib-v2/src/DynamicForm/components/FormTemplate/ObjectFieldTemplate.tsx
@@ -1,9 +1,9 @@
 import { Grid } from "@mui/material";
 
-export const CustomObjectFieldTemplate = ({ properties }) => {
+export const CustomObjectFieldTemplate = ({ properties }: any) => {
     return (
       <Grid container spacing={2}>
-        {properties.map((prop) => {
+        {properties.map((prop: any) => {
           const gridOptions = prop.content?.props?.uiSchema?.['ui:options']?.grid || {};
   
           return (
