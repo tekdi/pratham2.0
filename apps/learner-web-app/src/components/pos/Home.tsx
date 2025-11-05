ef5582fee82fa961fbbc4dd663668f40af5f3927
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2025-11-05 14:42:18 +0530

    update pr

--- apps/learner-web-app/src/components/pos/Home.tsx
+++ apps/learner-web-app/src/components/pos/Home.tsx
@@ -412,7 +412,7 @@ const rangeLength = contentLanguageField?.range?.length || 0;
             }}
           >
 <SpeakableText>
-  {videosCount+gamesCount+otherFormatsCount+15}
+  {videosCount+gamesCount+otherFormatsCount}
 </SpeakableText>
           </Typography>
           <Typography
