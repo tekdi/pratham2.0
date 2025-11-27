bfcd9282627bcb0eac276f103b576e52ba3fa257
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2025-11-27 08:08:24 +0530

    Added englishTitle support in list view in workspace

--- mfes/workspace/src/pages/workspace/content/publish/index.tsx
+++ mfes/workspace/src/pages/workspace/content/publish/index.tsx
@@ -102,6 +102,7 @@ const PublishPage = () => {
       image: item?.appIcon,
 
       name: item?.name,
+      englishName: item?.englishName,
       description: item?.description,
       language: item.contentLanguage ? item.contentLanguage : item?.language,
 
