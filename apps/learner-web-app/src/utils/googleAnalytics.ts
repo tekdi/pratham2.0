7329a9afc9a5187cbeb48819b25ceea8f1ae0236
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2026-02-24 18:02:08 +0530

    Issue feat: add content id, unit id and course id in analytic parameter events

--- apps/learner-web-app/src/utils/googleAnalytics.ts
+++ apps/learner-web-app/src/utils/googleAnalytics.ts
@@ -12,14 +12,32 @@ export const initGA = (measurementId: string) => {
 };
 
 export const logPageView = (url: string) => {
-  if(localStorage.getItem('userProgram'))
-  {
-    ReactGA.send({ hitType: "pageview", page: url , program: localStorage.getItem('userProgram')});
-    return;
-
+  // Extract doid from current route if present
+  const currentPath = window.location.pathname;
+  const doidMatches = Array.from(currentPath.matchAll(/do_\d+/g));
+  
+  const additionalParams: Record<string, string> = {};
+  if (doidMatches.length === 3) {
+    // If there are exactly 3 do_ IDs: first=content_id, second=course_id, third=unit_id
+    additionalParams.content_id = doidMatches[0][0];
+    additionalParams.course_id = doidMatches[1][0];
+    additionalParams.unit_id = doidMatches[2][0];
+  } else if (doidMatches.length > 0) {
+    // If there are other numbers of do_ IDs, use the last one as content_id
+    additionalParams.content_id = doidMatches[doidMatches.length - 1][0];
   }
-  else
-  ReactGA.send({ hitType: "pageview", page: url });
+  
+  const userProgram = localStorage.getItem('userProgram');
+  if(userProgram)
+  {
+    const pageViewData: Record<string, string> = { hitType: "pageview", page: url, program: userProgram, ...additionalParams };
+    ReactGA.send(pageViewData);
+    return;
+  }
+  else {
+    const pageViewData: Record<string, string> = { hitType: "pageview", page: url, ...additionalParams };
+    ReactGA.send(pageViewData);
+  }
 };
 
 export const logEvent = ({
@@ -54,6 +72,20 @@ export const logEvent = ({
   if (userProgram) {
     console.log('userProgram', userProgram);
     eventParams.program = userProgram;
+  }
+  
+  // Extract doid from current route if present
+  const currentPath = window.location.pathname;
+  const doidMatches = Array.from(currentPath.matchAll(/do_\d+/g));
+  
+  if (doidMatches.length === 3) {
+    // If there are exactly 3 do_ IDs: first=content_id, second=course_id, third=unit_id
+    eventParams.content_id = doidMatches[0][0];
+    eventParams.course_id = doidMatches[1][0];
+    eventParams.unit_id = doidMatches[2][0];
+  } else if (doidMatches.length > 0) {
+    // If there are other numbers of do_ IDs, use the last one as content_id
+    eventParams.content_id = doidMatches[doidMatches.length - 1][0];
   }
   
   // Use gtag directly to send event with custom parameters
