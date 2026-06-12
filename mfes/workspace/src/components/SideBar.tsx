fa3cb44eeaf4fddfdb79fce2eedf4d0a56be4829
rushi-tekdi <rushi_g@tekditechnologies.com>
2026-06-12 19:35:43 +0530

    hide button of content create

--- mfes/workspace/src/components/SideBar.tsx
+++ mfes/workspace/src/components/SideBar.tsx
@@ -119,11 +119,11 @@ const Sidebar: React.FC<SidebarProps> = ({ selectedKey, onSelect }) => {
           },
         ]
       : []),
-    {
-      text: 'Bulk Import',
-      key: 'bulk-import',
-      icon: <UploadFileOutlinedIcon />,
-    },
+    // {
+    //   text: 'Bulk Import',
+    //   key: 'bulk-import',
+    //   icon: <UploadFileOutlinedIcon />,
+    // },
   ];
 
   const handleNavigation = (key: string) => {
