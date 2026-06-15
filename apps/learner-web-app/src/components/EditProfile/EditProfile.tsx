87b494e6b92dbe11e267aaff947b6bf1f52efcae
Tejashri Majage <tejashri.majage@tekditechnologies.com>
2026-06-15 13:18:54 +0530

    update pr

--- apps/learner-web-app/src/components/EditProfile/EditProfile.tsx
+++ apps/learner-web-app/src/components/EditProfile/EditProfile.tsx
@@ -270,13 +270,15 @@ const EditProfile = ({ completeProfile, enrolledProgram, uponEnrollCompletion }:
 
           console.log('alterSchema', alterSchema);
           console.log('alterUISchema', alterUISchema);
-          // if(!alterSchema.properties.phone_type_accessible )
-          // {
-          //   delete alterSchema.properties.own_phone_check;
-          //   delete alterUISchema.own_phone_check;
-          //    alterSchema.required.pop('own_phone_check');
-          // }
-          // If enrolledProgram + completeProfile and there are no required fields left,
+          if(!alterSchema.properties.phone_type_accessible )
+          {
+            delete alterSchema.properties.own_phone_check;
+            delete alterUISchema.own_phone_check;
+            if(alterSchema.required.includes('own_phone_check')){
+             alterSchema.required.pop('own_phone_check');
+            }
+          }
+       //   If enrolledProgram + completeProfile and there are no required fields left,
           // the profile is already complete — skip the form and proceed directly.
           if (enrolledProgram && completeProfile && (!alterSchema?.required || alterSchema.required.length === 0)) {
             skipLoadingReset = true; // keep loader visible while handleAccessProgram runs
