b94c27ec644d34f845bc231b3b050ef9cbb2a36d
rushi-tekdi <rushi_g@tekditechnologies.com>
2026-06-17 14:12:19 +0530

    mpeg mimetype support added

--- mfes/players/public/libs/sunbird-content-player/preview/coreplugins/org.ekstep.videorenderer-1.1/renderer/plugin.js
+++ mfes/players/public/libs/sunbird-content-player/preview/coreplugins/org.ekstep.videorenderer-1.1/renderer/plugin.js
@@ -18,7 +18,7 @@ org.ekstep.contentrenderer.baseLauncher.extend({
     heartBeatData: {},
     enableHeartBeatEvent: false,
     _constants: {
-        mimeType: ["video/mp4", "video/webm", "audio/mp3"],
+        mimeType: ["video/mp4", "video/webm", "audio/mp3", "audio/wav", "audio/mpeg"],
         events: {
             launchEvent: "renderer:launch:video"
         }
@@ -49,7 +49,7 @@ org.ekstep.contentrenderer.baseLauncher.extend({
         var data = _.clone(content);
         this.heartBeatData.stageId = content.mimeType === 'video/x-youtube' ? 'youtubestage' : 'videostage';
         var globalConfigObj = EkstepRendererAPI.getGlobalConfig();
-        if(content.mimeType === "audio/mp3") {
+        if(content.mimeType === "audio/mp3" || content.mimeType === "audio/wav" || content.mimeType === "audio/mpeg") {
             skipValidation = true;
             data.streamingUrl = false;
             var regex = new RegExp("^(http|https)://", "i");
