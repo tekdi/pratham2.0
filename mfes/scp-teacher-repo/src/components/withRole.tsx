1250b286ff4c93970b1d18e0074cb42db1140f37
Your Alfaj2302 <alfaj.shaikh@tekditechnologies.com>
2026-07-27 10:41:56 +0530

    Add support for Second Chance Program Pathways across various components

--- mfes/scp-teacher-repo/src/components/withRole.tsx
+++ mfes/scp-teacher-repo/src/components/withRole.tsx
@@ -3,14 +3,19 @@ import { useEffect } from 'react';
 import { TENANT_DATA } from '../../app.config';
 
 const withRole =
-  (allowedRole: string) => (WrappedComponent: React.ComponentType) => {
+  (allowedRole: string | string[]) =>
+  (WrappedComponent: React.ComponentType) => {
+    const allowedRoles = Array.isArray(allowedRole)
+      ? allowedRole
+      : [allowedRole];
+
     return (props: any) => {
       const router = useRouter();
 
       useEffect(() => {
         const role = localStorage.getItem(TENANT_DATA.TENANT_NAME);
 
-        if (role !== allowedRole) {
+        if (!role || !allowedRoles.includes(role)) {
           router.push('/');
         }
       }, []);
