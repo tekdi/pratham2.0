c15fc386097e7808e807c558167f5b3952f1e970
Alfaj Shaikh <alfaj.shaikh@tekditechnologies.com>
2026-08-19 14:36:08 +0530

    Fix conditional rendering for user program check in MyComponent

--- apps/learner-web-app/src/components/Content/CommonL1ContentList.tsx
+++ apps/learner-web-app/src/components/Content/CommonL1ContentList.tsx
@@ -121,10 +121,10 @@ const MyComponent: React.FC<CommonL1ContentListProps> = ({ notab = false }) => {
       {isLogin && (
         <>
           {typeof window !== 'undefined' &&
-            localStorage.getItem('userProgram') ===
+            (localStorage.getItem('userProgram') ===
               TenantName.SECOND_CHANCE_PROGRAM ||
               localStorage.getItem('userProgram') ===
-                TenantName.SECOND_CHANCE_PROGRAM_PATHWAYS && (
+                TenantName.SECOND_CHANCE_PROGRAM_PATHWAYS) && (
               <Grid
                 container
                 spacing={2}
